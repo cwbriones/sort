f642760d148bcf5a4e2d5befad196ed5bc6bac43
Christian Briones <cwbriones@gmail.com>
2013-08-13 14:41:38 -0700

    Implemented cocktail sort

--- sort.cpp
+++ sort.cpp
@@ -6,10 +6,8 @@
 /*
  * TODO:
  * Insertion Sort
- * Bubble Sort
  * Merge Sort
  * Quick Sort
- * Cocktail Shaker sort
  */
 template <class T>
 void swap(T& a, T& b){
@@ -37,6 +35,30 @@ void bubble_sort(T* items, size_t size){
 }
 
 template <class T>
+void cocktail_sort(T* items, size_t size){
+    int left = 0;
+    int right = size - 2;
+
+    bool swapped = true;
+    while (left < right){
+        // Go up the list
+        for (int i = left; i <= right; i++){
+            if (items[i] > items[i + 1]){
+                swap(items[i], items[i + 1]);
+            }
+        }
+        left++;
+        // Go down the list
+        for (int i = right; i >= left; i--){
+            if (items[i-1] > items[i]){
+                swap(items[i-1], items[i]);
+            }
+        }
+        right--;
+    }
+}
+
+template <class T>
 void shuffle(T* items, size_t size){
     for (int i = size - 1; i > 0; i--){
         int j = rand() % i;
@@ -45,31 +67,15 @@ void shuffle(T* items, size_t size){
     return;
 }
 
-// template <>
-// void shuffle(int* items, size_t size){
-//     for (int i = size - 1; i > 0; i--){
-//         int j = rand() % i;
-//         items[i] = items[i] ^ items[j];
-//         items[j] = items[i] ^ items[j];
-//         items[i] = items[i] ^ items[j];
-//     }
-//     return;
-// }
-
 int main(int argc, const char *argv[])
 {
-    const int size = 10;
+    const int size = 100;
     int nums[size];
     for (int i = 0; i < size; i++){
         nums[i] = i + 1;
     }
     shuffle(nums, size);
-    for (auto x : nums){
-        std::cout << x << ' ';
-    }
-    std::cout << std::endl;
-
-    bubble_sort(nums, size);
+    cocktail_sort(nums, size);
     for (auto x : nums){
         std::cout << x << ' ';
     }
