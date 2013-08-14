06b447400fe34fba97604a840a8fceec4acf6060
Christian Briones <cwbriones@gmail.com>
2013-08-14 14:59:48 -0700

    Implemented quick sort

--- sort.cpp
+++ sort.cpp
@@ -5,10 +5,27 @@
 
 /*
  * TODO:
+ * Selection Sort
+ * Heap Sort
+ * Shell Sort
+ * Radix Sort
  * Insertion Sort
- * Merge Sort
  * Quick Sort
  */
+
+/*
+ * Function randint:
+ * Generates a uniformly distributed integer
+ * between the values and b inclusive
+ */
+int randint(int a, int b){
+    int N = b - a + 1;
+    int r;
+    do {
+        r = rand();
+    } while (r >= RAND_MAX - N && r >= N);
+    return a + (r % N);
+}
 
 template <class T>
 void swap(T& a, T& b){
@@ -17,11 +34,63 @@ void swap(T& a, T& b){
     b = tmp;
 }
 
-template <>
-void swap(int& a, int& b){
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
+// template <>
+// void swap(int& a, int& b){
+//     a = a ^ b;
+//     b = a ^ b;
+//     a = a ^ b;
+// }
+
+template <class T>
+void shuffle(T* items, size_t size){
+    for (int i = size - 1; i > 0; i--){
+        int j = rand() % i;
+        swap(items[i], items[j]);
+    }
+    return;
+}
+
+template <class T>
+size_t partition(T* items, size_t lo, size_t hi){
+    int i = lo;
+    int j = hi + 1;
+
+    // Randomly choose a partition element
+    int pivot = randint(lo, hi);
+    // Move the pivot to the front
+    swap(items[pivot], items[lo]);
+    while (true){
+        // Increase first pointer until elem greater than pivot
+        // and second pointer until elem less than pivot
+        while (items[++i] < items[lo] && i != hi);
+        while (items[lo] < items[--j] && j != lo);
+        if (i >= j){
+            break;
+        }
+        // Swap the smaller item with the larger item
+        swap(items[i], items[j]);
+    }
+    // Move the pivot back
+    swap(items[lo], items[j]);
+    return j;
+}
+
+template <class T>
+void quick_sort(T* items, int lo, int hi){
+    if (hi <= lo){
+        // Arrays of length <= 1 are sorted
+        return;
+    }
+    // Partition the array
+    int j = partition(items, lo, hi);
+    // Recursively sort subarrays
+    quick_sort(items, lo, j-1);
+    quick_sort(items, j+1, hi);
+}
+
+template <class T>
+void quick_sort(T* items, size_t size){
+    quick_sort(items, 0, size - 1);
 }
 
 template <class T>
@@ -80,7 +149,6 @@ void cocktail_sort(T* items, size_t size){
     int left = 0;
     int right = size - 2;
 
-    bool swapped = true;
     while (left < right){
         // Go up the list
         for (int i = left; i <= right; i++){
@@ -99,24 +167,15 @@ void cocktail_sort(T* items, size_t size){
     }
 }
 
-template <class T>
-void shuffle(T* items, size_t size){
-    for (int i = size - 1; i > 0; i--){
-        int j = rand() % i;
-        swap(items[i], items[j]);
-    }
-    return;
-}
-
 int main(int argc, const char *argv[])
 {
-    const int size = 100;
+    const int size = 50;
     int nums[size];
     for (int i = 0; i < size; i++){
         nums[i] = i + 1;
     }
     shuffle(nums, size);
-    merge_sort(nums, size);
+    quick_sort(nums, size);
     for (auto x : nums){
         std::cout << x << ' ';
     }
