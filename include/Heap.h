079e1760519c95203a0af2df9f3bcf2eba1bb8f0
Christian Briones <cwbriones@gmail.com>
2013-08-30 15:57:38 -0700

    Added licensing headers

--- include/Heap.h
+++ include/Heap.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2013 Christian Briones
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef MIN_HEAP_H_
 #define MIN_HEAP_H_
 
@@ -7,12 +29,8 @@
 
 static const int GROWTH_FACTOR = 2;
 
-
 template <class T>
 void swap(T& a, T& b){
-    if (a == b){
-        return;
-    }
     T tmp = a;
     a = b;
     b = tmp;
@@ -58,7 +76,7 @@ MinHeap<Value>::MinHeap(Value* items, size_t size){
     while (data_size < size){
         data_size *= GROWTH_FACTOR;
     }
-    data = new Value[data_size];
+    data = new Value[GROWTH_FACTOR];
     for (int i = 0; i < size; i++){
         insert(items[i]);
     }
@@ -112,7 +130,7 @@ void MinHeap<Value>::insert(Value value){
 template <class Value>
 void MinHeap<Value>::grow(){
     Value* new_data = new Value[data_size * GROWTH_FACTOR];
-    for (int i = 0; i < size_; i++){
+    for (int i = 0; i < data_size; i++){
         new_data[i] = data[i];
     }
     data_size *= GROWTH_FACTOR;
@@ -126,7 +144,7 @@ void MinHeap<Value>::shrink(){
         return;
     }
     Value* new_data = new Value[data_size / GROWTH_FACTOR];
-    for (int i = 0; i < size_; i++){
+    for (int i = 0; i < data_size; i++){
         new_data[i] = data[i];
     }
     data_size /= GROWTH_FACTOR;
@@ -141,13 +159,9 @@ Value MinHeap<Value>::min() const {
 
 template <class Value>
 Value MinHeap<Value>::pop_min(){
-    // The minimum is the head
     Value val = data[0];
-    --size_;
-    // Move the end of the heap to the front
-    if (size_ >= 1){
-        data[0] = data[size_];
-    }
+    size_--;
+    data[0] = data[size_];
     sift_down();
     if (size_ < data_size / GROWTH_FACTOR){
         shrink();
@@ -172,11 +186,10 @@ void MinHeap<Value>::sift_down(){
     size_t node = 0;
     while (node < size_){
         // If the current node has no children we are done
-        if (left(node) == npos && right(node) == npos){
+        if (left(node) < 0 && right(node) < 0){
             break;
         }
-        // Only child is to the left
-        else if (right(node) == npos && left(node) < size_){
+        else if (right(node) < 0 && left(node) < size_){
             if (data[node] > data[left(node)]){
                 swap(data[node], data[left(node)]);
                 node = left(node);
@@ -184,8 +197,7 @@ void MinHeap<Value>::sift_down(){
                 break;
             }
         }
-        // Only child is to the right
-        else if (left(node) == npos && right(node) < size_){
+        else if (left(node) < 0 && right(node) < size_){
             if (data[node] > data[right(node)]){
                 swap(data[node], data[right(node)]);
                 node = right(node);
@@ -193,8 +205,6 @@ void MinHeap<Value>::sift_down(){
                 break;
             }
         }
-        // We have two children at this node
-        // Pick the smaller one
         else {
             size_t child = data[left(node)] < data[right(node)] 
                 ? left(node) : right(node);
